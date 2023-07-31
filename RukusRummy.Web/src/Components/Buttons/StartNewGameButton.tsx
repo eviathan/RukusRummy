a6d738be1905df5ce88eb77df16d140f396f989a
Brian Williams <brian.williams@lbresearch.com>
2023-07-31 22:03:45 +0100

    EOD

--- RukusRummy.Web/src/Components/Buttons/StartNewGameButton.tsx
+++ RukusRummy.Web/src/Components/Buttons/StartNewGameButton.tsx
@@ -6,8 +6,7 @@ export const StartNewGameButton: React.FC<React.PropsWithChildren<{}>> = () => {
     const navigate = useNavigate();
 
     const handleClick = () => {
-        // TODO: This needs to call create session endpont then redirect to the session page with the id as the route
-        navigate('/iuakhsdjkusahdjkash');
+        navigate('/create-game');
     }
 
     return (
