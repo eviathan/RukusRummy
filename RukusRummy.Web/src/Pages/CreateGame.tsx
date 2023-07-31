c04d3aa01744ef5f7b545c1195f1011f59e30d41
Brian Williams <brian.williams@lbresearch.com>
2023-07-31 22:12:28 +0100

    Add rudimentary form to create game page

--- RukusRummy.Web/src/Pages/CreateGame.tsx
+++ RukusRummy.Web/src/Pages/CreateGame.tsx
@@ -1,10 +1,27 @@
 
 function CreateGame() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Create Game</h1>
-        </div>
+      <div className="create-game">
+        <h1>Create Game</h1>
+        <input placeholder="Game name"></input>
+        <select>
+          <option>Deck</option>
+        </select>
+        <select>
+          <option>Who can reveal cards: All Players</option>
+          <option>Who can manage issues: Just me</option>
+        </select>
+        <select>
+          <option>Who can manage issues: All players</option>
+          <option>Who can manage issues: Just me</option>
+        </select>
+        <p>Auto-reveal cards</p>
+        <input type="checkbox" />
+        <p>Enable fun features</p>
+        <input type="checkbox" />
+        <p>Show average in the results</p>
+        <input type="checkbox" />
+        <button className="primary">Create Game</button>
       </div>
     );
   }
